57b4be241ba939a0e7bc10940fd9b7696db89f46
Austin <austinofficial007@gmail.com>
2023-07-29 17:42:51 +0530

    greeting

---
+++ dev.js
@@ -0,0 +1,15 @@
+const time = new Date();
+const presentTime = time.getHours();
+var displayText = "hi";
+
+if (presentTime > 4 && presentTime < 12) {
+    displayText = "Good Morning!";
+} else if(presentTime >= 12 && presentTime < 5){
+    displayText = "Good Afternoon!";
+} else{
+    displayText = "Good Evening!"
+}
+
+console.log(displayText);
+
+document.getElementById("greeting").innerHTML = displayText;
