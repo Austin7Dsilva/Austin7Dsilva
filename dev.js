2714aae1d07bea74952b9a9b64c76b04e1b89ff0
Austin <austinofficial007@gmail.com>
2023-07-29 17:45:32 +0530

    Update dev.js

--- dev.js
+++ dev.js
@@ -4,7 +4,7 @@ var displayText = "hi";
 
 if (presentTime > 4 && presentTime < 12) {
     displayText = "Good Morning!";
-} else if(presentTime >= 12 && presentTime < 5){
+} else if(presentTime >= 12 && presentTime < 17){
     displayText = "Good Afternoon!";
 } else{
     displayText = "Good Evening!"
